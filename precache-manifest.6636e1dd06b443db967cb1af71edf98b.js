bc46cfd5793205dc4bef9b02af1c14a8cb5ddd79
Abdul Rauf <raufyy4@gmail.com>
2022-07-31 16:17:49 +0500

    Updates

---
+++ precache-manifest.6636e1dd06b443db967cb1af71edf98b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d534fec378cb42745ae2ddc047869381",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d73129ac02278a9964b9",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "8b846fa236b9dc456c4f",
+    "url": "./static/js/2.1b518a16.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.1b518a16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d73129ac02278a9964b9",
+    "url": "./static/js/main.45ab1ee5.chunk.js"
+  },
+  {
+    "revision": "01c86b0fe964cc2c5d64",
+    "url": "./static/js/runtime-main.c7a4da8c.js"
+  }
+]);
